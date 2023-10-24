f149705db81dec5676b4f103b5ef23aeb478691e
Bappi Saha <bappi35-2097@diu.edu.bd>
2023-10-24 18:56:09 +0600

    some page added

--- app/(Auth)/signin/page.tsx
+++ app/(Auth)/signin/page.tsx
@@ -40,46 +40,105 @@ const LoginForm = () => {
     setFormValues({ ...formValues, [name]: value })
   }
 
-  const input_style =
-    "form-control block w-full px-4 py-5 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-
   return (
-    <div className='h-screen flex items-center justify-center'>
-      <form onSubmit={onSubmit} className='w-96'>
-        {error && (
-          <p className='text-center bg-red-300 py-4 mb-6 rounded'>{error}</p>
-        )}
-        <div className='mb-6'>
-          <input
-            required
-            type='email'
-            name='email'
-            value={formValues.email}
-            onChange={handleChange}
-            placeholder='Email address'
-            className={`${input_style}`}
-          />
-        </div>
-        <div className='mb-6'>
-          <input
-            required
-            type='password'
-            name='password'
-            value={formValues.password}
-            onChange={handleChange}
-            placeholder='Password'
-            className={`${input_style}`}
-          />
-        </div>
-        <button
-          type='submit'
-          style={{ backgroundColor: `${loading ? "#ccc" : "#3446eb"}` }}
-          className='inline-block px-7 py-4 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full'
-          disabled={loading}
-        >
-          {loading ? "loading..." : "Sign In"}
-        </button>
-      </form>
+    <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+      <div
+        className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
+        aria-hidden='true'
+      >
+        <div
+          className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
+      <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+        <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+        <form className='space-y-6' onSubmit={onSubmit}>
+          {error && (
+            <p className='text-center bg-red-300 py-4 mb-6 rounded'>{error}</p>
+          )}
+          <div>
+            <label
+              htmlFor='email'
+              className='block text-sm font-medium leading-6 text-gray-900'
+            >
+              Email address
+            </label>
+            <div className='mt-2'>
+              <input
+                id='email'
+                name='email'
+                type='email'
+                autoComplete='email'
+                required
+                value={formValues.email}
+                onChange={handleChange}
+                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className='flex items-center justify-between'>
+              <label
+                htmlFor='password'
+                className='block text-sm font-medium leading-6 text-gray-900'
+              >
+                Password
+              </label>
+              <div className='text-sm'>
+                <a
+                  href='#'
+                  className='font-semibold text-indigo-600 hover:text-indigo-500'
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className='mt-2'>
+              <input
+                id='password'
+                name='password'
+                type='password'
+                autoComplete='current-password'
+                required
+                value={formValues.password}
+                onChange={handleChange}
+                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type='submit'
+              disabled={loading}
+              style={{ backgroundColor: `${loading ? "#ccc" : "#3446eb"}` }}
+              className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+            >
+              {loading ? "loading..." : "Sign In"}
+            </button>
+          </div>
+        </form>
+
+        <p className='mt-10 text-center text-sm text-gray-500'>
+          Not a member?{" "}
+          <a
+            href='/signup'
+            className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
+          >
+            Sign up for free
+          </a>
+        </p>
+      </div>
     </div>
   )
 }
